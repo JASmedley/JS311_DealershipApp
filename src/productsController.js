412616a30eb3e146a06e9529ba409f972d978f85
JASmedley <jasmine.a.smedley@gmail.com>
2023-11-07 18:58:03 -0600

    finishing the project

--- src/productsController.js
+++ src/productsController.js
@@ -4,8 +4,35 @@ let db = require("../data/products")
 let getProducts = function(req,res){
   res.json(db);
 }
-let getProduct = function(req,res){}
-let addProduct = function(req,res){}
+let getProduct = function(req,res){
+  let id = req.params.id;
+  let found;
+  for(let i=0;i<db.length;i++){
+      let item = db[i];
+      if(item._id == id){
+          found = item;
+      }
+  }
+  
+  res.json(found)
+}
+let addProduct = function(req,res){
+  let product = req.body;
+  let next = db.length+1;
+  product._id = next
+  let newProduct = {
+    _id: product._id,
+    name: product.name,
+    description: product.description,
+    rating: product.rating,
+    imgURL: product.imgURL,
+    price: product.price,
+    category: product.category,
+    reviews: product.reviews
+}
+  db.push(newProduct);
+  res.json(newProduct);
+}
 module.exports = {
   getProducts,
   getProduct,
--- src/vehiclesController.js
+++ src/vehiclesController.js
@@ -4,8 +4,37 @@ let db = require("../data/vehicles")
 let getVehicles = function(req,res){
   res.json(db);
 }
-let getVehicle = function(req,res){}
-let addVehicle = function(req,res){}
+let getVehicle = function(req,res){
+  let id = req.params.id;
+  let found;
+  for(let i=0;i<db.length;i++){
+      let item = db[i];
+      if(item._id == id){
+          found = item;
+      }
+  }
+  res.json(found)
+}
+let addVehicle = function(req,res){
+  let vehicle = req.body;
+  let next = db.length+1;
+  vehicle._id = next
+  let newVehicle = {
+    _id: vehicle._id,
+    imgURL: vehicle.imgURL,
+    year: vehicle.year,
+    make: vehicle.make,
+    model: vehicle.model,
+    price: vehicle.price,
+    km: vehicle.km,
+    miles: vehicle.miles,
+    fuel: vehicle.fuel,
+    city: vehicle.city,
+    isNew: vehicle.isNew
+}
+  db.push(newVehicle);
+  res.json(newVehicle);
+}
 module.exports = {
   getVehicles,
   getVehicle,
